458a1638d95f6853b28e162c595b1dda73a8051f
Alexey Khrameev <a.khrameev@it.ecommpay.com>
2023-01-30 08:48:44 +0300

    version 0.8.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-    implementation("com.ecommpay:msdk-core-android:0.8.0")
+    implementation("com.ecommpay:msdk-core-android:0.8.1")
 
     implementation("androidx.startup:startup-runtime:1.1.1")
 
