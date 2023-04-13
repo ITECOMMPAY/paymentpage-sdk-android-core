57230c1f3fedae008be906ec8f4344fa34d137a3
Roman Karpilenko <r.karpilenko@it.ecommpay.com>
2023-04-13 15:01:26 +0300

    version 0.8.8

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-    implementation("com.ecommpay:msdk-core-android:0.8.7")
+    implementation("com.ecommpay:msdk-core-android:0.8.8")
 
     implementation("androidx.startup:startup-runtime:1.1.1")
 
